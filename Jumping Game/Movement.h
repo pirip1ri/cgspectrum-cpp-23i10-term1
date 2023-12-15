b23abcc3a9a5f5ca8898bb13974be8fc4a1eb278
Matt Pirrie <mattpirrie01@gmail.com>
2023-12-15 10:54:33 +0000

    Replacing Jumping Game with up-to-date Jumping Game

--- Jumping Game/Movement.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class Movement
-{
-public:
-	void MovingUp(int& newPlayerX, int& newPlayerY, bool& m_FloorBelow, bool& m_Jumping, bool& m_floating);
-	void MovingLeft(int& newPlayerX, int& newPlayerY, bool& m_FloorBelow, bool& m_Jumping, bool& m_floating);
-	void MovingRight(int& newPlayerX, int& newPlayerY, bool& m_FloorBelow, bool& m_Jumping, bool& m_floating);
-};
